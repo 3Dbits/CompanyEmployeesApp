b165995c093b087a8bbc7dda4707752d6655be20
Matija Cvetan <matija.cvetan@t.ht.hr>
2023-09-24 13:35:27 +0200

    GET/POST collections

--- Presentation/Controllers/CompaniesController.cs
+++ Presentation/Controllers/CompaniesController.cs
@@ -37,5 +37,21 @@ namespace CompanyEmployees.Presentation.Controllers
 
             return CreatedAtRoute("CompanyById", new { id = createdCompany.Id }, createdCompany);
         }
+
+        [HttpGet("collection/({ids})", Name = "CompanyCollection")]
+        public IActionResult GetCompanyCollection(IEnumerable<Guid> ids)
+        {
+            var companies = _service.CompanyService.GetByIds(ids, trackChanges: false);
+
+            return Ok(companies);
+        }
+
+        [HttpPost("collection")]
+        public IActionResult CreateCompanyCollection([FromBody] IEnumerable<CompanyForCreationDto> companyCollection)
+        {
+            var (companies, ids) = _service.CompanyService.CreateCompanyCollection(companyCollection);
+
+            return CreatedAtRoute("CompanyCollection", new { ids }, companies);
+        }
     }
 }
