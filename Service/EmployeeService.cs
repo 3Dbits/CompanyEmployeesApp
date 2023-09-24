d8b92e96779af6f25a99c9f55cd052cc198c57f4
Matija Cvetan <matija.cvetan@t.ht.hr>
2023-09-24 11:32:27 +0200

    ExceptionMiddleware & new controllers

--- Service/EmployeeService.cs
+++ Service/EmployeeService.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using Contracts;
+using Entities.Exceptions;
 using Service.Contracts;
+using Shared.DataTransferObjects;
 
 namespace Service
 {
@@ -15,6 +17,29 @@ namespace Service
             _repository = repository;
             _logger = logger;
             _mapper = mapper;
+        }
+
+        public IEnumerable<EmployeeDto> GetEmployees(Guid companyId, bool trackChanges)
+        {
+            _ = _repository.Company.GetCompany(companyId, trackChanges)
+                ?? throw new CompanyNotFoundException(companyId);
+
+            var employeesFromDb = _repository.Employee.GetEmployees(companyId, trackChanges);
+            var employeesDto = _mapper.Map<IEnumerable<EmployeeDto>>(employeesFromDb);
+
+            return employeesDto;
+        }
+
+        public EmployeeDto GetEmployee(Guid companyId, Guid id, bool trackChanges)
+        {
+            _ = _repository.Company.GetCompany(companyId, trackChanges) 
+                ?? throw new CompanyNotFoundException(companyId);
+
+            var employeeDb = _repository.Employee.GetEmployee(companyId, id, trackChanges) 
+                ?? throw new EmployeeNotFoundException(id);
+
+            var employee = _mapper.Map<EmployeeDto>(employeeDb);
+            return employee;
         }
     }
 }
